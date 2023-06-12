43f37f748ba8a3c6cd68e1efa7eeef538fcb9aaa
Artem Dukhnitskiy <16158503+dukhniav@users.noreply.github.com>
2023-06-11 22:01:24 -0600

    added market data source to worker

--- bottie/worker.py
+++ bottie/worker.py
@@ -11,48 +11,79 @@ class Worker:
     def __init__(self) -> None:
         logger.info(f"Initializing worker (ver={__version__})")
         self._stop_event = threading.Event()
-        self._worker_thread = None  # Initialize the worker thread variable
-
-    # def start(self) -> bool:
-    #     logger.info("Starting worker...")
-    #     self._stop_event.clear()
-    #     self._worker_thread = threading.Thread(target=self._worker_loop)
-    #     self._worker_thread.start()
-    #     status = True
-    #     return status
+        self._worker_thread = None
+        self.md = None  # Variable to hold the market data source
 
     def start(self) -> bool:
         logger.info("Starting worker...")
-        self._worker_loop
-        status = True
-        return status
+        self._stop_event.clear()
+        self._worker_thread = threading.Thread(target=self._worker_loop)
+        self._worker_thread.start()
+        return True
 
-    def stop(self):
+    def stop(self) -> bool:
         logger.info("Stopping worker...")
-        if self._worker_thread:
-            self._worker_thread.join()  # Wait for the worker thread to finish
+        self._stop_event.set()
+        self._worker_thread.join()  # Wait for the worker thread to finish
+        self._worker_thread = None  # Reset the worker thread variable
+        return True
 
     def market_data_source(self, source):
         self.md = source
 
-    # def stop(self) -> bool:
-    #     logger.info("Stopping worker...")
-    #     self._stop_event.set()
-    #     self._worker_thread.join()  # Wait for the worker thread to finish
-    #     self._worker_thread = None  # Reset the worker thread variable
-    #     status = True
-    #     return status
-
-    # def _worker_loop(self):
-
-    #     while not self._stop_event.is_set():
-    #         # Perform worker tasks
-    #         time.sleep(1)  # Example: Simulate some work
-    #     logger.info("Worker stopped.")
-
     def _worker_loop(self):
-
         while not self._stop_event.is_set():
-            # Perform worker tasks
-            time.sleep(1)  # Example: Simulate some work
+            # Retrieve market data from the data source (Finnhub API)
+            market_data = self.md.get_market_data()
+
+            # Perform analysis on market data
+            # (Implement your own analysis logic here)
+            signals = self.analyze_market_data(market_data)
+
+            # Generate orders based on the signals
+            orders = self.generate_orders(signals)
+
+            # Execute the generated orders
+            self.execute_orders(orders)
+
+            time.sleep(1)  # Sleep for some time before the next iteration
+
         logger.info("Worker stopped.")
+
+    def analyze_market_data(self, market_data):
+        # Perform analysis on the market data
+        # Implement your own analysis logic here
+        signals = []
+        # Example: Generate a random signal for demonstration purposes
+        if market_data:
+            signal = {
+                'ticker': market_data['ticker'],
+                'signal_type': 'buy',
+                'price': market_data['price'],
+                'timestamp': market_data['timestamp']
+            }
+            signals.append(signal)
+        return signals
+
+    def generate_orders(self, signals):
+        # Generate orders based on the signals
+        # Implement your own order generation logic here
+        orders = []
+        # Example: Generate a market buy order for each signal
+        for signal in signals:
+            order = {
+                'ticker': signal['ticker'],
+                'order_type': 'market',
+                'action': 'buy',
+                'quantity': 1
+            }
+            orders.append(order)
+        return orders
+
+    def execute_orders(self, orders):
+        # Execute the generated orders
+        # Implement your own order execution logic here
+        for order in orders:
+            # Example: Print the order details
+            logger.info(f"Executing order: {order}")
+            # Execute the order using the appropriate API or method
